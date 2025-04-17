8bd152d54f58bd4c3da14fd8717da47f0efd55e3
Ghost <kali@localhost>
2025-04-17 12:28:46 +0000

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
 import './styles/index.css';
-import MatrixBackground from './components/MatrixBackground';
 
 // Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import CyberCursor from './components/CyberCursor';
+import MatrixBackground from './components/MatrixBackground';
+import ParallaxBackground from './components/ParallaxBackground';
 
 // Pages
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
- import ProjectsPage from './pages/ProjectsPage';
+import ProjectsPage from './pages/ProjectsPage';
 import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
     <div className="App bg-cyber-black min-h-screen crt">
+      {/* Global Effects */}
       <MatrixBackground opacity={0.05} density="low" speed="medium" />
+      <ParallaxBackground />
+      <CyberCursor />
+      
+      {/* Navigation */}
       <Navbar />
       
+      {/* Main Content */}
       <main>
-          <HomePage />  
+        <HomePage />
         <AboutPage />
-         <ProjectsPage />
-       <ContactPage />  
+        <ProjectsPage />
+        <ContactPage />
       </main>
       
+      {/* Footer */}
       <Footer />
     </div>
   );
